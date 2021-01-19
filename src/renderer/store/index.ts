795c089ae2d4fdd02761eb1b3c09efb8c20c5a73
tighug <kapuriko123@gmail.com>
2021-01-19 11:04:15 +0900

    feat: Add a gateway of lines

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -2,9 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import feeders from "./feeders";
 import nodes from "./nodes";
+import lines from "./lines";
 import cases from "./cases";
 
-const reducer = combineReducers({ feeders, nodes, cases });
+const reducer = combineReducers({ feeders, nodes, lines, cases });
 const configuredStore = configureStore({ reducer });
 
 export type RootState = ReturnType<typeof reducer>;
