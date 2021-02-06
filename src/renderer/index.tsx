c6f3d69ca8444fefc0bb254a07c44917f200793d
tighug <kapuriko123@gmail.com>
2021-02-06 19:27:33 +0900

    feat: Update index.tsx

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,18 +2,25 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import Home from "./components/pages/Home";
+import Home from "./components/page/Home";
+import dark from "./theme/dark";
+import {
+  StylesProvider,
+  ThemeProvider as MuiThemeProvider,
+} from "@material-ui/core/styles";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import "normalize.css";
-import "./index.css";
-import { theme } from "./theme";
-import { ThemeProvider } from "@material-ui/core";
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Home />
-      </ThemeProvider>
+      <MuiThemeProvider theme={dark}>
+        <StyledThemeProvider theme={dark}>
+          <StylesProvider injectFirst>
+            <Home />
+          </StylesProvider>
+        </StyledThemeProvider>
+      </MuiThemeProvider>
     </Provider>,
     document.getElementById("root")
   );
