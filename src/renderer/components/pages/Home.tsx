139b8c31eeaf981046650f93fa979a829458439c
tighug <kapuriko123@gmail.com>
2021-02-06 19:24:10 +0900

    refactor: Rename the page folder

--- src/renderer/components/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DefaultLayout from "../layouts/DefaultLayout";
-import MainPanel from "../organisms/MainPanel";
-import SidePanel from "../organisms/SidePanel";
-
-export default function Home(): JSX.Element {
-  return <DefaultLayout side={<SidePanel />} main={<MainPanel />} />;
-}
